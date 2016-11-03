61ed6470cb1c972cfa63d48e99bd2da13ac8a235
André Hickmann <andre.hickmann@swepp.eu>
2016-11-03 10:30:24 +0100

    minor changes

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
+'use strict';
+
 Package.describe({
   name: 'higg:cache',
   summary: 'A basic cache with a cache-storage for persisting data in the memory of your application.',
   version: '0.0.1',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/andrehickmann/meteor-cache.git',
   documentation: 'README.md'
 });
 
